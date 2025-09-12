0a0d548982008d4f6a39577faf3940e73bf7e901
JakubStaniec <jakubstaniec1@gmail.com>
2025-09-12 13:19:14 +0200

    a

--- syntax.php
+++ syntax.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>syntax</title>
 </head>
+
 <body>
     <h1>1</h1>
-<?php echo 'Tu'. ' funkcja'. ' echo' ?> <br>
-<?php echo 'Tu funkcja echo' ?> <br>
-<?= 'Tu'. ' funkcja'. ' echo' ?><br>
-<?= 'Tu funkcja echo' ?> <br>
-<h1>2</h1>
-<?php print "Zgłasza się funkcja print"?> <br>
-<?php print "Zgłasza się funkcja print"?> <br>
-<h1>3</h1>
-<?= 'swędzi mnie łydka' ?><br>
-<?= 'głaszczę kota po głowie' ?><br>
+    <?php echo 'Tu' . ' funkcja' . ' echo' ?> <br>
+    <?php echo 'Tu funkcja echo' ?> <br>
+    <?= 'Tu' . ' funkcja' . ' echo' ?><br>
+    <?= 'Tu funkcja echo' ?> <br>
+    <h1>2</h1>
+    <?php print "Zgłasza się funkcja print" ?> <br>
+    <?php print "Zgłasza się funkcja print" ?> <br>
+    <h1>3</h1>
+    <?= 'swędzi mnie łydka' ?><br>
+    <?= 'głaszczę kota po głowie' ?><br>
 
 
-<!-- lubię komentować -->
-<?php // komentuje komentuje ?>
+    <!-- lubię komentować -->
+    <?php // komentuje komentuje 
+    ?>
 
-<?php /*
+    <?php /*
 lubię
 mojego
 kota
 */ ?>
 
 </body>
+
 </html>
