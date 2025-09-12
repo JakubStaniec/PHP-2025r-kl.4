ae0dd8ca9793ae463fd6775af14d14be602df291
JakubStaniec <jakubstaniec1@gmail.com>
2025-09-12 12:45:59 +0200

    yyy

---
+++ syntax.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>1</h1>
+<?php echo 'Tu'. ' funkcja'. ' echo' ?> <br>
+<?php echo 'Tu funkcja echo' ?> <br>
+<?= 'Tu'. ' funkcja'. ' echo' ?><br>
+<?= 'Tu funkcja echo' ?> <br>
+<h1>2</h1>
+<?php print "Zgłasza się funkcja print"?> <br>
+<?php print "Zgłasza się funkcja print"?> <br>
+<h1>3</h1>
+<?= 'swędzi mnie łydka' ?><br>
+<?= 'głaszczę kota po głowie' ?><br>
+<h1>4</h1>
+<!-- lubię komentować -->
+<?php // komentuje komentuje?>
+
+<?php /*
+lubię
+mojego
+kota
+*/?>
+
+</body>
+</html>
