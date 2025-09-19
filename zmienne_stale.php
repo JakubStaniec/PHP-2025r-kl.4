21311e569878b4b7c5d7f346e5245012dd42cb10
JakubStaniec <jakubstaniec1@gmail.com>
2025-09-19 11:42:48 +0200

    a

--- zmienne_stale.php
+++ zmienne_stale.php
@@ -10,17 +10,18 @@
     $liczba = "102" ;
     $zmienna = "1,25" ; 
     $bool = true ?>
-
+<hr>
 <?php echo 'Echo dla ciągu znaków: ' , $slowo ?> <br>
 <?php echo 'echo dlaliczby całkowitej: ' , $liczba ?> <br>
 <?php echo 'echo dla liczby rzeczywistej: ' , $zmienna ?> <br>
 <?php echo 'echo dlawartości bool-owskiej: ' , $bool ?> <br>
+<hr>
 <br>
 <?php echo 'var_dump dla ciągu znaków: '; var_dump($slowo) ?><br>
 <?php echo 'var_dump dla liczby całkowitej: '; var_dump($liczba) ?><br>
 <?php echo 'var_dump dla liczby rzeczywistej: '; var_dump($zmienna) ?><br>
 <?php echo 'var_dump dla wartości bool-owskiej: '; var_dump($bool) ?><br>
-
+<hr>
 <br>
 <php
 define("ROK",  "1993");
