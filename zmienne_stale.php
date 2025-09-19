22db38e5c923a37a8a03b9d1a06e8af29e7e29bf
JakubStaniec <jakubstaniec1@gmail.com>
2025-09-19 12:27:06 +0200

    yy

--- zmienne_stale.php
+++ zmienne_stale.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>zmienne stale</title>
+    <style>
+        span {
+        border: dotted;
+        color: orangered;
+        text-shadow: 1px, 4px, 4px blueviolet;
+        }
+    </style>
 </head>
 <body>
     <?php $slowo = "slowo1" ; 
@@ -11,27 +18,27 @@
     $zmienna = "1,25" ; 
     $bool = true ?>
 <hr>
-<?php echo 'Echo dla ciągu znaków: ' , $slowo ?> <br>
-<?php echo 'echo dlaliczby całkowitej: ' , $liczba ?> <br>
-<?php echo 'echo dla liczby rzeczywistej: ' , $zmienna ?> <br>
-<?php echo 'echo dlawartości bool-owskiej: ' , $bool ?> <br>
+Echo dla ciągu znaków:<span><?php echo $slowo ?> </span> <br>
+echo dlaliczby całkowitej:<span><?php echo  $liczba ?> </span> <br>
+echo dla liczby rzeczywistej:<span> <?php echo  $zmienna ?> </span> <br>
+echo dlawartości bool-owskiej:<span> <?php echo  $bool ?> </span><br>
 <hr>
 <br>
-<?php echo 'var_dump dla ciągu znaków: '; var_dump($slowo) ?><br>
-<?php echo 'var_dump dla liczby całkowitej: '; var_dump($liczba) ?><br>
-<?php echo 'var_dump dla liczby rzeczywistej: '; var_dump($zmienna) ?><br>
-<?php echo 'var_dump dla wartości bool-owskiej: '; var_dump($bool) ?><br>
+var_dump dla ciągu znaków:<span><?php echo var_dump($slowo) ?></span><br>
+var_dump dla liczby całkowitej:<span><?php echo var_dump($liczba) ?></span><br>
+var_dump dla liczby rzeczywistej:<span><?php echo var_dump($zmienna) ?></span><br>
+var_dump dla wartości bool-owskiej:<span><?php echo var_dump($bool) ?></span><br>
 <hr>
 <br>
-<php
-define("ROK",  "1993");
-define("gra" , "WarThunder"); ?>
-<?php echo 'print dla pierwszej stałej'?>
+<?php
+define("ROK" , "1993");
+define("FILM" , "StarWars"); ?>
+<p> print dla pierwszej stałej: <span><?php print ROK ?></span></p>
+<p> print dla drugiej stałej: <span><?php print FILM ?></span></p>
 <php print ROK ?> <br>
 <hr>
-
-
-
-
+<p>var_export dla pierwszej stałej: <?php var_export(ROK) ?> </p> 
+<p>var_export dla drugiej stałej: <?php var_export(FILM) ?> </p>
+<hr>
 </body>
 </html>
