9c9fe683ef10143b1f1aaada4705bf75ce921b8c
JakubStaniec <jakubstaniec1@gmail.com>
2025-09-19 12:46:44 +0200

    zad4

---
+++ paragraph.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>zad4</title>
+</head>
+<body>
+    <?php $a = 97;
+    $b = 3.14;
+    $c = 'PHP';
+    ?>
+<h3 style="color: red;"><?php echo $a ?> jest liczbą pierwszą </h3>
+<p style="color: #336ee1";>Liczba Pi zaokrąglona do dwóch miejsc po przecinku: <?php echo $b ?> </p>
+<div style="color: darkgreen; text-decoration:underline overline; color:green"><?php echo $c; ?> to język skryptowy wykonywany po stronie serwera 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
