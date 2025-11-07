5b3b8794461c88adfb4ac2b78dd84c2d63668a61
JakubStaniec <jakubstaniec1@gmail.com>
2025-11-07 12:45:29 +0100

    a

---
+++ pole_trojkata.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a = 12345679;
+    $g = 144;
+    ?>
+<address>
+ Pole trójkąta o podstawie długości <?=$a?> i wyskości równej <?=$g?>, wynosi <?= $a * $g * 1/2 ?> J<sup>2</sup>
+</address>
+
+
+</body>
+</html>
