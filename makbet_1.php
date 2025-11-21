930740780f99382470a023aaa10cfe4edfd635eb
JakubStaniec <jakubstaniec1@gmail.com>
2025-11-21 13:19:34 +0100

    gg

---
+++ makbet_1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zad 1</title>
+</head>
+<body>
+<p><q> Niech będzie co będzie; czas wszystko równo w swym unosi pędzie. </q><p>
+</body>
+</html>
---
+++ pole_trojkata_rownobocznego.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>zad 2</title>
+    <style>
+        .tak{
+            color: darkorange;
+        }
+fieldset{
+
+border-left: 25px;
+border-right: 25px;
+border-radius: 1px;
+padding: 1px;
+}
+
+
+        </style>
+</head>
+<body>
+    <?php
+    $a = 17
+    ?>
+
+<p class="tak"><fieldset>Pole trójkąta równobocznego o boku długości <?=$a?> jest równe j<sup>2</sup>.</fieldset></p>
+
+
+
+</body>
+</html>
