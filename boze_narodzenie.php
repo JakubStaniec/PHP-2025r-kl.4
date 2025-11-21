9b2d2f1025c24c1c3b60bfff588141f281bfe8cc
JakubStaniec <jakubstaniec1@gmail.com>
2025-11-21 14:37:27 +0100

    oki

---
+++ boze_narodzenie.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>yes</title>
+</head>
+<body>
+    <li>Pierwszy dzień świąt Bożego Narodzenia w tym roku wypada w <?= date("l" , mktime(0,0,0,12,25,2025)) ?> </li>
+   <li> Drugi dzień świąt Bożego Narodzenia w tym roku wypada w  <?= date("l" , mktime(0,0,0,12,26,2025)) ?><li>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
