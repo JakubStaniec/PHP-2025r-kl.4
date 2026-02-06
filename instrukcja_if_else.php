cf844f5eb2f1fe4a64583b03b6dddd7ff584c1cc
JakubStaniec <jakubstaniec1@gmail.com>
2026-02-06 13:12:47 +0100

    06.02

---
+++ instrukcja_if_else.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+h2{
+text-align: center;
+color: darkorchid;
+
+}
+h4{
+    text-align: center;
+    color: magenta;
+}
+
+
+    </style>
+</head>
+<body>
+
+    <?php
+    $losowa = rand(1, 30);
+
+?>
+<h2><?=$losowa  ?> </h2>
+
+
+<h4><?php
+if ($losowa >=20) echo"Wylosowana liczba jest większa lub równa 20"?></h4> 
+
+<h3 style="text-align: center; color: navy;"><?php if ($losowa % 2 == 0) {echo"Wylosowana liczba jest parzysta";}    
+else {echo"Wylosowana liczba jest nieparzysta";}?></h3>
+
+<h4 style="text-align: center; color: deeppink;"> 
+<?php
+if ($losowa <=10)
+{echo "pierwsza dziesiątka";}
+elseif ($losowa <= 20)
+{echo "druga dziesiątka";}
+else {echo "trzecia dziesiątka";}
+
+?></h4>
+
+
+
+
+</body>
+</html>
