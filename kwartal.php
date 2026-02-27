f2729989da807ee242c4ca2393cd35b125da3c75
JakubStaniec <jakubstaniec1@gmail.com>
2026-02-27 11:57:05 +0100

    a

---
+++ kwartal.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<style>
+h1{
+    border-top: 4px dotted pink;
+    text-align: center;
+    border-bottom: 4px dotted pink;
+}
+
+</style>
+
+
+</head>
+<body>
+    <h1>
+    <?php
+    $miesiac = rand(1, 1.5) ;
+    print($miesiac);
+    ?>
+    </h1>
+    <h3>
+        <?php
+        switch($miesiac){
+        
+        
+        } 
+        ?>
+    </h3>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
