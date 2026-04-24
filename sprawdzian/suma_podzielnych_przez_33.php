167d3e848c85961eb4e22d19e22978af73832c4a
JakubStaniec <jakubstaniec1@gmail.com>
2026-04-24 11:40:37 +0200

    o

--- sprawdzian/suma_podzielnych_przez_33.php
+++ sprawdzian/suma_podzielnych_przez_33.php
@@ -5,14 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>zad 2</title>
 </head>
-
 <body>
-    <h3>Liczby dwucyfrowe podzielne przez 33 to:</h3>
+Liczby dwucyfrowe podzielne przez 33 to:
     <?php
     
-$range = range(-33,33,);
+$range = range(-33,99);
 
-foreach ( $range as $cyfra)  
+foreach ($range as $cyfra)  
 
 if ( $cyfra % 33 == 0 )
 {echo "<span>$cyfra</span>,";
@@ -23,19 +22,6 @@ else {
 }
 if ($cyfra === 33)
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<strong>Suma liczb dwucyfrowych podzielnych przez 33 jest równa <?php (198) ?>
 </body>
 </html>
